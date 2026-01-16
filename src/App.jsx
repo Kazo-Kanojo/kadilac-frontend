fdd7501e6afab79a081ea5b465e87de4ba55fe59
Enzo <enzopyton24@gmail.com>
2026-01-16 16:28:34 -0300

    Rstruturando as pastas

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        my project kadlack(hello world)
+      </div>
+    </>
+  )
+}
+
+export default App
